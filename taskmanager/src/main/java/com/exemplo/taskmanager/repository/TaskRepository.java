c84dd3e7cb1b21ba0a46a361243f71fa79047f87
Samuel_Dg <samueldgomes00@gmail.com>
2025-03-27 19:36:42 -0300

    Primeiro commit

---
+++ taskmanager/src/main/java/com/exemplo/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.exemplo.taskmanager.repository;
+
+import com.exemplo.taskmanager.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
